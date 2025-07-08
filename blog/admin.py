769815c2cf5f49e1dcaa1aeaeb3e0d8bed522463
Litde <wdzieczny.michal.2002@gmail.com>
2025-07-08 16:30:10 +0200

    Commitig with errors with blog_app

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import Tag, Author, BlogPost
 
 # Register your models here.
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email')
+    search_fields = ('first_name', 'last_name', 'email')
+
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'get_author', 'created_at', 'updated_at', 'get_tags')
+    search_fields = ('title', 'author__first_name', 'author__last_name')
+    list_filter = ('created_at', 'updated_at', 'tags')
+    filter_horizontal = ('tags',)  # Nice widget for many-to-many tags
+    
+    def get_tags(self, obj):
+        """Display tags in list view"""
+        return ", ".join([tag.name for tag in obj.tags.all()])
+    get_tags.short_description = 'Tags'
+    
+    def get_author(self, obj):
+        """Display full author name in list view"""
+        return f"{obj.author.first_name} {obj.author.last_name}"
+    get_author.short_description = 'Author'
+
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(BlogPost, BlogPostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,52 @@
 from django.db import models
 
 # Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+    
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    bio = models.TextField(blank=True, null=True)
+    
+    def __str__(self):
+        return self.full_name()
+    
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=200)
+    excerpt = models.CharField(max_length=500, blank=True, null=True)
+
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='posts')
+    tags = models.ManyToManyField(Tag, blank=True, related_name='posts')
+    
+    content = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    image = models.ImageField(upload_to='blog_images/', blank=True, null=True)
+
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True)
+
+    class Meta:
+        ordering = ['-created_at']
+        verbose_name_plural = "Blog Posts"
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = self.title.lower().replace(' ', '-')
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+    
+
